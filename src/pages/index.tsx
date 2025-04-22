34231cba697c23d0595437f5180f93fcdf4e37b5
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-22 14:08:33 -0300

    add: tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <h1>App</h1>
+    </div>
+  );
+}
