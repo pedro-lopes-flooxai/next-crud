00726ce48cb589e5d441e6f4bbaa87846229cbfd
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-22 17:59:20 -0300

    add: editar and excluir

--- src/core/Cliente.ts
+++ src/core/Cliente.ts
@@ -1,22 +1,27 @@
-export default class Cliente { 
-    #id: string 
-    #nome: string 
-    #idade: number 
+export default class Cliente {
+    #id: string
+    #nome: string
+    #idade: number
 
-    constructor(nome: string, idade: number, id: string = null) { 
-        this.#nome = nome 
-        this.#idade idade 
-        this.#id = id 
+    constructor (nome: string, idade: number, id: string = null ) {
+        this.#nome = nome
+        this.#idade = idade
+        this.#id = id
     }
 
-    static vazio() { 
-        return new Cliente('', 0) 
-    } 
-
-    get id() { 
-        return this.#id 
-    } 
-    
-    get nome() { 
-        return this.#nome 
+    static vazio() {
+        return new Cliente('', 0)
     }
+
+    get id() {
+        return this.#id
+    }
+
+    get nome() {
+        return this.#nome
+    }
+
+    get idade() {
+        return this.#idade
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,38 @@
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
-import Layout from "../components/Layout"; 
-import Tabela from "../components/Tabela"; 
-import Cliente from "../core/Cliente"; 
+export default function Home() {
 
-export default function Home() { 
-
-const clientes = [ 
-    new Cliente('Ana', 34, '1')
+const clientes = [
+  new Cliente('Ana',34,'1'),
+  new Cliente('Bia',34,'2'),
+  new Cliente('Carlos',34,'3'),
+  new Cliente('Pedro',34,'4'),
 ]
 
-return ( 
+function clienteSelecionado(cliente: Cliente) {
+  console.log(cliente.nome)
+
+}
+
+function clienteExcluido(cliente: Cliente) {
+  console.log(cliente.nome)
+
+}
+
+  return (
     <div className={`
-        flex justify-center items-center h-screen 
-        bg-gradient-to-rfrom-blue-500to-purple-500 
-        text-white 
-    `}> 
-    <Layout titulo="Cadastro Simples"> 
-        <Tabela clientes></Tabela> 
-    </Layout> 
-    </div> 
-    )
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes} 
+        clienteSelecionado={clienteSelecionado}
+        clienteExcluido={clienteExcluido}
+          />
+      </Layout>
+    </div>
+  );
 }
